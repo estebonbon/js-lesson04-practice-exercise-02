d069cc79c3c9640fe711502ddc721b6dae0f7ab8
estebonbon <estesarmiento1@gmail.com>
2024-01-22 10:45:25 -0500

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+var instaIcon = document.querySelector(".fa-instagram");
+instaIcon.style.color = "#d45bb9";
+
+var facebookIcon = document.querySelector(".fa-facebook");
+facebookIcon.style.color = "#6a99b5";
+
+var border = document.querySelector(".contact p");
+border.style.border = "1px solid #d45bb9";
+border.style.padding = "1em";
